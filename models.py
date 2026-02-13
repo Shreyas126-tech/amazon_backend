efbace3634c0e81ae1c23ef37104ba5131d77f3a
Shreyas126-tech <pshreyas191@gmail.com>
2026-02-13 14:49:47 +0530

    commit

--- models.py
+++ models.py
@@ -1,1 +1,12 @@
 from db import Base
+
+class user(base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    email = Column(String)
+    password = Column(String)
+    address = Column(String)
+    phone = Column(Integer)
+    alt_phone = Column(Integer)
+    isprime = Column(Boolean)
